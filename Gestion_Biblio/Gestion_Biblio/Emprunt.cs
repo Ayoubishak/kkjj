e500a982ac4b80e5fa1ee6321308364b78ea8ea3
Haytham EL FADLAOUI <fdlhaytham@gmail.com>
2019-04-10 02:32:07 +0100

    Ajout de classe Emprunt

---
+++ Gestion_Biblio/Gestion_Biblio/Emprunt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Biblio
+{
+    class Emprunt
+    {
+        SqlConnection cnx = new SqlConnection(Properties.Settings.Default.Biblio);
+        private DateTime datejour;
+        private DateTime dateretour;
+        private Usager usager;
+        private Exemplaire exemplaire;
+
+        public DateTime Datejour { get => datejour; set => datejour = value; }
+        public DateTime Dateretour { get => dateretour; set => dateretour = value; }
+        internal Usager Usager { get => usager; set => usager = value; }
+        internal Exemplaire Exemplaire { get => exemplaire; set => exemplaire = value; }
+
+        public Emprunt()
+        {
+        }
+
+        public Emprunt(DateTime datejour, DateTime dateretour, Usager usager, Exemplaire exemplaire)
+        {
+            this.Datejour = datejour;
+            this.Dateretour = dateretour;
+            this.Usager = usager;
+            this.Exemplaire = exemplaire;
+        }
+
+        public Emprunt Identfiant(int idu, int ide, DateTime dj)
+        {
+            Emprunt e = new Emprunt();
+            return e;
+        }
+       
+    }
+}
